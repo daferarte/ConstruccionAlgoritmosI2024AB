c8d8c4adeb10422eef843d79094af541e164c00e
daferarte <daferarte@gmail.com>
2024-02-23 15:43:06 -0500

    inicio repositorio

---
+++ Empleados/Empleado.py
@@ -0,0 +1,35 @@
+class Empleado:
+    #aqui va el codigo
+    
+    '''-------------------------------------
+    # Atributos
+    -------------------------------------'''
+    
+    nombres=''
+    apellidos=''
+    '''-------------------------------------
+    # 1 = Masculino y 2 = Femenino
+    -------------------------------------'''
+    sexo=0
+    salario=0
+    
+    '''----------------------------------------------------------------
+    # Metodos
+    ----------------------------------------------------------------'''
+    
+    def CambiarSalario(self, nSalario):
+        #aqui va el codigo
+        self.salario = nSalario
+        return "Su nuevo salario es: " + self.salario
+    
+    def ConsultarSalario(self):
+        # Aqui va el codigo
+        return self.salario
+    
+    def AumentoSalario(self):
+        # aqui va el codigo
+        aumento = self.salario*0.05
+        nSalario = self.salario+aumento
+        self.salario = nSalario
+        return "Su nuevo salario es: " + self.salario
+        
